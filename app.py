97f654678613056c40b4d0ea1a5ae4021d3f8d37
d0n0dind0n <140164617+d0n0dind0n@users.noreply.github.com>
2026-05-12 14:46:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def process_row(row):
     name = str(row['Name Surname']).lower().strip()
     full_text = f"{purpose} {name}"
     
-    cat, div, sub = "Services", "YF Main", ""
+    cat, div, sub = "", "", ""
 
     # Category Logic
     if any(kw in full_text for kw in ["ziedojum", "ziedot"]): cat = "Donations"

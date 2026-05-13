8e56da163b927acc9ca2c5694663a3e8e3050a73
d0n0dind0n <140164617+d0n0dind0n@users.noreply.github.com>
2026-05-13 11:34:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def process_row(row):
     elif any(kw in full_text for kw in ["reimbursement", "psd", "erasmus"]):
         cat = "Erasmus+"
     
-    elif any(kw in full_text for kw in ["biedru nauda", "dalības", "dalibmaksa", "klubu", "dalībmaksa"]) or name in membership_lookup:
+    elif any(kw in full_text for kw in ["biedru nauda", "dalības", "dalibmaksa", "klubu", "dalībmaksa", "FY"]) or name in membership_lookup:
         cat = "Membership"
     
     elif any(kw in full_text for kw in ["bolt", "citybee", "noma", "komisija", "internetbank", "ikea", "depo", "kartes mēneša maksa", "Pavadzīme", "Tele2", "KENG"]):

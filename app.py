62a49d7ab9d2a150286498b12e3bcae83295610c
d0n0dind0n <140164617+d0n0dind0n@users.noreply.github.com>
2026-05-13 10:50:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def process_row(row):
     # Category Logic
     if any(kw in full_text for kw in ["ziedojum", "ziedot"]): cat = "Donations"
     elif any(kw in full_text for kw in ["alga", "stipendija", "autoratl", "līguma", "8.3-8.1"]): cat = "Salaries"
-    elif "erasmus" in full_text or "reimbursement" or "PSD2" in full_text: cat = "Erasmus+"
+    elif any(kw in full_text for kw in ["reimbursement", "PSD2", "erasmus"]): cat = "Erasmus+"
     elif any(kw in full_text for kw in ["biedru nauda", "dalības", "dalibmaksa"]) or name in membership_lookup:
         cat = "Membership"
     elif any(kw in full_text for kw in ["bolt", "citybee", "noma", "komisija", "internetbank", "ikea", "depo", "Kartes mēneša maksa"]):
